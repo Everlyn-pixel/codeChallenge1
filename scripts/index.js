7888d72208955fe945f7149ad6c1362be3bc8e68
root <root@DESKTOP-0E4VTFB.localdomain>
2025-03-18 11:09:30 +0300

    added a list of names in javascript

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const ramens = [
+  {
+    id: 1,
+    name: "Shoyu Ramen",
+    restaurant: "Ichiran",
+    image: "shoyu.jpg",
+    rating: 5,
+    comment: "Delicious!",
+  },
+  {
+    id: 2,
+    name: "Miso Ramen",
+    restaurant: "Menya",
+    image: "miso.jpg",
+    rating: 4,
+    comment: "Very flavorful!",
+  },
+  {
+    id: 3,
+    name: "Tonkotsu Ramen",
+    restaurant: "Ramen-ya",
+    image: "tonkotsu.jpg",
+  },
+];
+
+// display ramens in ramens menu
+
+const displayRamen = (ramen) => {
+let section = document.getElementById("ramen-menu");
